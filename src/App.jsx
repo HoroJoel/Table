c06446f1578d6e3a848565e0925c69c9d4f75a81
JoelAravena <joel.aravena@cencosud.cl>
2024-03-21 18:01:08 -0300

    ship: Title

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { data } from "./mock";
 function App() {
   return (
     <>
-      <h1 className=" text-lg  bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 m-4">
+      <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 m-4">
         List account
       </h1>
       <AccountTable data={data} />
